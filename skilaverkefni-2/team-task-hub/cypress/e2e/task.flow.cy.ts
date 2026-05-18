645312f5f2a7724b8a14d959c7c8ef1c31adbdf0
Your Name <youremail@example.com>
2026-05-18 13:21:20 +0000

    cypress

---
+++ skilaverkefni-2/team-task-hub/cypress/e2e/task.flow.cy.ts
@@ -0,0 +1,20 @@
+/// <reference types="cypress" />
+
+describe("Projects flow", () => {
+
+  it("creates a new project", () => {
+
+    cy.visit("http://localhost:5173");
+
+    cy.window().then((win) => {
+      cy.stub(win, "prompt").returns("Team Task Hub");
+    });
+
+    cy.contains("Add project").click();
+
+    cy.contains("Team Task Hub")
+      .should("exist");
+
+  });
+
+});
