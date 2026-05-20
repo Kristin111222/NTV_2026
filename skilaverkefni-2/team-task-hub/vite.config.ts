e21fd5a78a2949cf0fd6c9312e32ef35b327b912
Your Name <youremail@example.com>
2026-05-20 09:34:51 +0000

    fix bug in vitest setup

--- skilaverkefni-2/team-task-hub/vite.config.ts
+++ skilaverkefni-2/team-task-hub/vite.config.ts
@@ -16,7 +16,8 @@ export default defineConfig({
       extends: true,
       test: {
         globals: true,
-        environment: "jsdom"
+        environment: "jsdom",
+         setupFiles: "./src/test/setup.ts",
       }
     }, {
       extends: true,
