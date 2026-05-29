d39817d987392e7eb0441400df0480f134d8353f
Your Name <youremail@example.com>
2026-05-29 14:17:42 +0000

    url and key added to supabase.ts

--- Lokaverkefni/TourGuide/src/services/supabase.ts
+++ Lokaverkefni/TourGuide/src/services/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'SETTU_URL_HÉR'
-const supabaseKey = 'SETTU_ANON_KEY_HÉR'
+const supabaseUrl = 'https://ampgjtiuisqkjanopwmz.supabase.co/rest/v1/'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFtcGdqdGl1aXNxa2phbm9wd216Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk3OTM2NjEsImV4cCI6MjA5NTM2OTY2MX0.N-yplTYwQsjUjqbY89wcxocA0dZL_3RhZ7Lm-lkgMQ4'
 
 export const supabase = createClient(
   supabaseUrl,
