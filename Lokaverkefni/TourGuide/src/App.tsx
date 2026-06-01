3b8bbd2aa40788a0cf6e2794d8e6ecc2e1622165
Your Name <youremail@example.com>
2026-06-01 19:17:07 +0000

    Bucket in supabase turned to public

--- Lokaverkefni/TourGuide/src/App.tsx
+++ Lokaverkefni/TourGuide/src/App.tsx
@@ -4,6 +4,7 @@ import { getProducts } from './services/products'
 function App() {
 
   const [products, setProducts] = useState<any[]>([])
+  const [selectedCategory, setSelectedCategory] = useState(0)
 
   useEffect(() => {
     async function loadProducts() {
@@ -14,17 +15,59 @@ function App() {
     loadProducts()
   }, [])
 
+  const filteredProducts =
+    selectedCategory === 0
+      ? products
+      : products.filter(
+          (product) => product.category_id === selectedCategory
+        )
+
   return (
     <div>
-      <h1>My Store</h1>
+      <h1>Tour Guide</h1>
 
-      <div>
-        {products.map((product) => (
-          <div key={product.id}>
-            <h2>{product.name}</h2>
-            <p>${product.price}</p>
-          </div>
+      <div className="category-buttons">
+
+        <button onClick={() => setSelectedCategory(0)}>
+          All
+        </button>
+
+        <button onClick={() => setSelectedCategory(1)}>
+          Adventure Tours
+        </button>
+
+        <button onClick={() => setSelectedCategory(2)}>
+          Northern Lights Tours
+        </button>
+
+        <button onClick={() => setSelectedCategory(3)}>
+          Museums & Culture
+        </button>
+
+      </div>
+
+      <div className="products-grid">
+
+        {filteredProducts.map((product) => (
+       <div className="product-card" key={product.id}>
+
+  <img
+    src={product.image_url}
+    alt={product.name}
+    className="product-image"
+  />
+
+  <h2>{product.name}</h2>
+
+  <p>${product.prize}</p>
+
+  <p>{product.trip_date}</p>
+
+  <button>Add to cart</button>
+
+</div>
         ))}
+
       </div>
     </div>
   )
